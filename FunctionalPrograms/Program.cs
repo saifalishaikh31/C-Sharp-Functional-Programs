744af6c027b605a25e307815a1a3513bbb22bca8
saifalishaikh31 <saifalishaikh31>
2022-01-26 11:11:14 +0530

    [Saif Ali] Add: Two Dimensional Array Added

---
+++ FunctionalPrograms/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FunctionalPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine(" 1.Two Dimensional Array."
+                    + "\n 2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1: TwoDArray twodimarray = new TwoDArray();
+                        twodimarray.TwoDimArray();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                    default: Console.WriteLine("Choose Correct option");
+                        break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
