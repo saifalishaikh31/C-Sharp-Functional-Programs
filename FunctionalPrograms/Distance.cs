5dc81bb57248e8c91a953d346e7b53afcd6fbfd7
saifalishaikh31 <saifalishaikh31>
2022-01-26 12:34:14 +0530

    [Saif Ali] Add: Distance Program Added

---
+++ FunctionalPrograms/Distance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FunctionalPrograms
+{
+    class Distance
+    {
+        public void FindDistance()
+        {
+            Console.WriteLine("Enter x distance");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y distance");
+            int y = Convert.ToInt32(Console.ReadLine());
+            double cal_distance = Math.Sqrt(x*x + y*y);
+            Console.WriteLine("distance from (" + x + ", " + y + ") to (0, 0) = " + cal_distance);
+        }
+    }
+}
